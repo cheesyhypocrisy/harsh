97d4a454e923310f005167323f660c31ff2ce92f
Harsh Mantri <24585585+cheesyhypocrisy@users.noreply.github.com>
2025-06-07 03:01:20 +0530

    Add support for reading history from $HISTFILE on startup

--- cmd/harsh/main.go
+++ cmd/harsh/main.go
@@ -4,6 +4,7 @@ import (
   "os"
   "fmt"
   "strings"
+  "bufio"
 
   "github.com/cheesyhypocrisy/harsh/internal/executor"
 	"github.com/cheesyhypocrisy/harsh/internal/shell"
@@ -12,6 +13,27 @@ import (
 func main() {
   path := os.Getenv("PATH")
   executor.PathDirs = append(executor.PathDirs, strings.Split(path, ":")...)
+
+  histfile, exists := os.LookupEnv("HISTFILE")
+  // Only enable history persistence if HISTFILE is set
+  if exists {
+    file, err := os.Open(histfile)
+    if err != nil {
+      fmt.Fprintf(os.Stderr, "Unable to read history from file %s with err: %#v\n", histfile, err.Error())
+      return
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+      executor.Hist = append(executor.Hist, scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+      fmt.Fprintf(os.Stderr, "Unable to read history from file %s with err: %#v\n", histfile, err.Error())
+    }
+  }
+
 
   if err := shell.Shell(); err != nil {
     fmt.Fprint(os.Stderr, err)
